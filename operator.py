f8cbd6571956aa934d2b070221a831105f63bc94
OHSEONGKWON <163419519+OHSEONGKWON@users.noreply.github.com>
2024-03-16 17:34:48 +0900

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,9 @@
+print(2**3) # 2^3 = 8
+print(5%3) # 나머지 2
+print(5//3) #몫 1
+
+print((3 > 0) and (3 < 5)) # True
+print((3 > 0) & (3 < 5)) # True
+
+print((3 > 0) or (3 > 5)) # True
+print((3 > 0) | (3 > 5)) # True
