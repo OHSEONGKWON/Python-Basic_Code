9bc824ebd5149ada55693740df08f78fe34ce9e5
OHSEONGKWON <163419519+OHSEONGKWON@users.noreply.github.com>
2024-03-16 17:34:01 +0900

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,11 @@
+#애완동물 소개
+
+animal = "고양이"
+name = "해피"
+age = 4
+hobby = "낮잠"
+is_adult = age > 3
+
+print("우리집 " + animal +"의 이름은 " + name +"예요")
+print(name +"은 " + str(age) +"살이며 " + hobby +"를 좋아해요.")
+print(name +"는 어른일까요 ? : " + str(is_adult))
